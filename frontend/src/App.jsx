75eae37e99e1142a36229a19f12f59d8daac3a76
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-04-06 21:45:10 +0530

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./containers/Home";
+const App = () => {
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='/*' element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
+      <Router>
+        <App />
+      </Router>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
