442be8dde486d0f91deb9cd75b19589620622557
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-04-06 22:04:47 +0530

    ADD: check if user is not logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Login from "./components/Login";
-import Home from "./containers/Home";
+
+import { Login } from "./components";
+import Home from "./container/Home";
+
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!User) navigate("/login");
+  }, []);
+
   return (
     <Routes>
       <Route path='login' element={<Login />} />
